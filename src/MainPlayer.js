b885d79abc26f6a82dd0e456dae452b9e38c6fc0
philly4toast <phillipyoun@gmail.com>
2020-05-27 03:20:33 -0700

    Works how I want it to, might change vidlist

--- src/MainPlayer.js
+++ src/MainPlayer.js
@@ -4,9 +4,16 @@ import React from 'react';
 class MainPlayer extends React.Component {
 
   render () {
+    var videoId = 'kIRab-Rn_Ik';
+    if (this.props.vidInfo){
+
+      console.log('mainplayer properties', this.props.vidInfo.id)
+      videoId = this.props.vidInfo.id.videoId
+    }
+
     return (<div className='mainplayer'>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/kIRab-Rn_Ik" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+<iframe width="560" height="315" src={"https://www.youtube.com/embed/" + videoId} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     )
   }
