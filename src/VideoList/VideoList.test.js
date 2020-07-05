9050285f5315b2867efc5fd16b6d552455021662
philly4toast <phillipyoun@gmail.com>
2020-07-04 22:13:09 -0700

    Loaded data via curl request
    Need to figure out how to test data, not text, inside a component

--- src/VideoList/VideoList.test.js
+++ src/VideoList/VideoList.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { render } from '@testing-library/react';
 import VideoList from './VideoList';
+import MEGANSTALsearch from '../zzzDATAsearchFill/MEGANSTALsearch'
 // import { isTSAnyKeyword } from '@babel/types';
 
 it('renders VideoList without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<VideoList/>, div);
+  ReactDOM.render(<VideoList musicVideos={MEGANSTALsearch.items}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
